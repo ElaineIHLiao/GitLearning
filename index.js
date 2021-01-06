8924f62c4f6c4252324cfc23db1372d70abf61d2
ELAINE LIAO <eliao@tw.ibm.com>
2021-01-06 15:21:40 +0800

    0106_A

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+0106_A
+
 git clone https://github.com/ElaineIHLiao/GitLearning.git
 
 
@@ -9,7 +11,16 @@ git push -u origin
 
 建立分支
 $ git branch <branchname>
+
 查看目前有那些分支
 $ git branch
+
 切換分支
 $ git checkout <branchname>
+
+將某個分支merge進master
+$ git checkout master
+$ git merge <branchname>
+
+刪除分支
+$ git branch -d <branchname>
